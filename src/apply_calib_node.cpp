fabf3af0fed130fe5570422d6e2d5128e744decc
SinfonIA <sinfonia@uniandes.edu.co>
2026-04-08 17:08:31 -0500

    update for ros2 jazzy

--- src/apply_calib_node.cpp
+++ src/apply_calib_node.cpp
@@ -1,8 +1,12 @@
 #include <rclcpp/rclcpp.hpp>
 #include "imu_calib/apply_calib.hpp"  // Adjust path based on your package structure
+#include <clocale>                    // <-- Added for locale control
 
 int main(int argc, char **argv)
 {
+  // Force the C++ parser to use a period (.) for decimals instead of a comma (,)
+  std::setlocale(LC_NUMERIC, "C");
+
   rclcpp::init(argc, argv);
 
   auto node = std::make_shared<imu_calib::ApplyCalib>();
